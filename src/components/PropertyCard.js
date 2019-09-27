ac4733d3e79e6a5389ecdc222330b6d2d5b8db32
Indra Lukmana <indralukmana@live.com>
2019-09-27 23:49:16 +0700

    ✨new: styling structure on card data

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -7,7 +7,7 @@ const property = {
   imageAlt: 'Rear view of modern home with pool',
   beds: 3,
   baths: 2,
-  title: 'Modern home in city center',
+  title: 'Modern home in city center with beautiful views',
   priceInCents: 190000,
   formattedPrice: '$1,900.00',
   reviewCount: 34,
@@ -16,17 +16,27 @@ const property = {
 
 export const PropertyCard = () => {
   return (
-    <div className='bg-gray-200 min-h-screen p-8 flex items-center justify-center'>
-      <div className='bg-white rounded-lg overflow-hidden border'>
-        <img src={property.imageUrl} alt={property.imageAlt} />
-        <div className='p-6'>
-          <h4>{property.title}</h4>
-          <div>
-            {property.beds} beds &bull; {property.baths}
-          </div>
-          <div>{property.formattedPrice} / wk</div>
-          <div>
-            {property.rating}/5 stars (based on {property.reviewCount} reviews)
+    <div className='antialiased text-gray-900'>
+      <div className='bg-gray-200 min-h-screen p-8 flex items-center justify-center'>
+        <div className='bg-white rounded-lg overflow-hidden border'>
+          <img src={property.imageUrl} alt={property.imageAlt} />
+          <div className='p-6'>
+            <div className='text-gray-600 text-xs uppercase font-semibold tracking-wide'>
+              {property.beds} beds &bull; {property.baths} baths
+            </div>
+            <h4 className='font-semibold text-lg leading-tight truncate'>
+              {property.title}
+            </h4>
+            <div className='mt-1'>
+              {property.formattedPrice}{' '}
+              <span className='text-gray-600 text-sm'>/ wk</span>
+            </div>
+            <div className='mt-4'>
+              <span className='text-teal-600'>{property.rating}/5 stars </span>
+              <span className='text-gray-600 text-sm'>
+                (based on {property.reviewCount} reviews)
+              </span>
+            </div>
           </div>
         </div>
       </div>
