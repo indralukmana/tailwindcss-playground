ee887191b305cc7bcb18bbe20e53da1aa305166e
Indra Lukmana <indralukmana@live.com>
2019-09-28 01:44:05 +0700

    ✨new: add depth with stacking element

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -18,47 +18,49 @@ export const PropertyCard = () => {
   return (
     <div className='antialiased text-gray-900'>
       <div className='bg-gray-200 min-h-screen p-8 flex items-center justify-center'>
-        <div className='bg-white rounded-lg overflow-hidden border'>
+        <div>
           <div className='relative bg-red-500 pb-2/3'>
             <img
-              className='absolute h-full w-full object-cover'
+              className='absolute h-full w-full object-cover rounded-lg shadow-md'
               src={property.imageUrl}
               alt={property.imageAlt}
             />
           </div>
-          <div className='p-6'>
-            <div className='flex items-baseline'>
-              <span className='inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide'>
-                new
-              </span>
-              <div className='ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide'>
-                {property.beds} beds &bull; {property.baths} baths
+          <div className='relative px-4 -mt-20'>
+            <div className='p-6 bg-white rounded-lg shadow-lg'>
+              <div className='flex items-baseline'>
+                <span className='inline-block bg-teal-200 text-teal-800 text-xs px-2 rounded-full uppercase font-semibold tracking-wide'>
+                  new
+                </span>
+                <div className='ml-2 text-gray-600 text-xs uppercase font-semibold tracking-wide'>
+                  {property.beds} beds &bull; {property.baths} baths
+                </div>
               </div>
-            </div>
-            <h4 className='mt-1 font-semibold text-lg leading-tight truncate'>
-              {property.title}
-            </h4>
-            <div className='mt-1'>
-              {property.formattedPrice}{' '}
-              <span className='text-gray-600 text-sm'>/ wk</span>
-            </div>
-            <div className='mt-2 flex items-center'>
-              {[...Array(5)].map((val, index) => (
-                <svg
-                  key={index}
-                  viewBox='0 0 24 24'
-                  className={`h-4 w-4 fill-current ${
-                    index + 1 <= property.rating
-                      ? 'text-teal-500'
-                      : 'text-teal-300'
-                  }`}
-                >
-                  <path d='M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z' />
-                </svg>
-              ))}
-              <span className='ml-2 text-gray-600 text-sm'>
-                {property.reviewCount} reviews
-              </span>
+              <h4 className='mt-1 font-semibold text-lg leading-tight truncate'>
+                {property.title}
+              </h4>
+              <div className='mt-1'>
+                {property.formattedPrice}{' '}
+                <span className='text-gray-600 text-sm'>/ wk</span>
+              </div>
+              <div className='mt-2 flex items-center'>
+                {[...Array(5)].map((val, index) => (
+                  <svg
+                    key={index}
+                    viewBox='0 0 24 24'
+                    className={`h-4 w-4 fill-current ${
+                      index + 1 <= property.rating
+                        ? 'text-teal-500'
+                        : 'text-teal-300'
+                    }`}
+                  >
+                    <path d='M8.128 19.825a1.586 1.586 0 0 1-1.643-.117 1.543 1.543 0 0 1-.53-.662 1.515 1.515 0 0 1-.096-.837l.736-4.247-3.13-3a1.514 1.514 0 0 1-.39-1.569c.09-.271.254-.513.475-.698.22-.185.49-.306.776-.35L8.66 7.73l1.925-3.862c.128-.26.328-.48.577-.633a1.584 1.584 0 0 1 1.662 0c.25.153.45.373.577.633l1.925 3.847 4.334.615c.29.042.562.162.785.348.224.186.39.43.48.704a1.514 1.514 0 0 1-.404 1.58l-3.13 3 .736 4.247c.047.282.014.572-.096.837-.111.265-.294.494-.53.662a1.582 1.582 0 0 1-1.643.117l-3.865-2-3.865 2z' />
+                  </svg>
+                ))}
+                <span className='ml-2 text-gray-600 text-sm'>
+                  {property.reviewCount} reviews
+                </span>
+              </div>
             </div>
           </div>
         </div>
